e1e754380a9adff4da6db0a2b7b45a9d24dbe69a
Gavin Pereira <gavineditz@gmail.com>
2021-03-11 19:48:06 +0530

    placed slides data in json

--- js/home.js
+++ js/home.js
@@ -1,11 +1,25 @@
 var defaultPath = '../img/';
-var imagePaths = ['desktop-image-hero-1.jpg', 'desktop-image-hero-2.jpg', 'desktop-image-hero-3.jpg'];
-var titles = ['Discover innovative ways to decorate', 'We are available all across the globe', 'Manufactured with the best materials'];
-var desc = [
-    "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-    "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-    "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+var slides = [
+    {
+        "title": 'Discover innovative ways to decorate',
+        "desc": "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+        "image": 'desktop-image-hero-1.jpg'
+    },
+    {
+        "title": 'We are available all across the globe',
+        "desc": "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+        "image": 'desktop-image-hero-2.jpg'
+    },
+    {
+        "title": 'Manufactured with the best materials',
+        "desc": "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+        "image": 'desktop-image-hero-3.jpg'
+    }
 ];
+
+
+
+
 
 var counter = 0;
 var header = document.querySelector('header');
@@ -21,29 +35,29 @@ function change(con) {
     n = counter % 3;
     if (n < 0)
         n = -n;
-    header.style.background = 'url(' + defaultPath + imagePaths[n] + ') no-repeat center center/cover';
+    header.style.background = 'url(' + defaultPath + slides[n].image + ') no-repeat center center/cover';
 
     setTimeout(function () {
         a.style.opacity = 0;
-    }, 300);
+    }, 200);
     setTimeout(function () {
         p.style.opacity = 0;
-    }, 600);
+    }, 400);
     setTimeout(function () {
         h1.style.opacity = 0;
-    }, 900);
+    }, 600);
 
     setTimeout(function () {
-        h1.innerHTML = titles[n];
+        h1.innerHTML = slides[n].title;
         h1.style.opacity = 1;
-    }, 1500);
+    }, 1000);
     setTimeout(function () {
-        p.innerHTML = desc[n];
+        p.innerHTML = slides[n].desc;
         p.style.opacity = 1;
-    }, 1800);
+    }, 1200);
     setTimeout(function () {
         a.style.opacity = 1;
-    }, 2100);
+    }, 1400);
 }
 
 
